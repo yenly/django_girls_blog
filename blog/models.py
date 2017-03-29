2669d37d75e8f528a7f8ddb9256cd65a48588f5c
Yenly Ma <yencodes@gmail.com>
2017-03-29 11:57:37 -0700

    Add moderation in comment system.

--- blog/models.py
+++ blog/models.py
@@ -29,5 +29,8 @@ class Comment(models.Model):
         self.approved_comment = True
         self.save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
     def __str__(self):
         return self.text
