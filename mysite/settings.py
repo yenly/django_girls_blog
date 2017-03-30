29f7a992979748b4842942ea9038cfee193aa247
Yenly Ma <yencodes@gmail.com>
2017-03-29 20:43:56 -0700

    Change db engine value

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'djangogirls',
         'USER': 'yenly',
         'PASSWORD': '',
