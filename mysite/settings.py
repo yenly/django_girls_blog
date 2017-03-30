51077e80460736d9a998ec2698f70c5a7b46f102
Yenly Ma <yencodes@gmail.com>
2017-03-29 20:37:34 -0700

    Remove debug flag.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'cc2a#4ejjlbff&xrv4^m4ak^r^*34(c@-ed@ldxd=6mn_htieu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
